dd53794305f87fd87644a56cfb9d3b1ad1c23667
David Muchow <93739747+davidmuchow@users.noreply.github.com>
2023-01-16 14:52:04 -0600

    attempt #2

--- setup.sh
+++ setup.sh
@@ -1,21 +1,8 @@
-#!/bin/sh
-RELEASE=3.7.4
- 
-# install dependencies
-sudo apt-get install libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev uuid-dev libffi-dev
- 
-# The following line is required for Buster but will fail harmlessly under Stretch
-sudo apt-get install libgdbm-compat-dev
- 
-# download and build Python
-mkdir ~/python3
-cd ~/python3
-wget https://www.python.org/ftp/python/$RELEASE/Python-$RELEASE.tar.xz
-tar xvf Python-$RELEASE.tar.xz
-cd Python-$RELEASE
-./configure --enable-shared
-make
+wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz
+tar -zxvf Python-3.9.9.tgz
+cd Python-3.9.9
 sudo make altinstall
-sudo ldconfig
-sudo rm -rf ~/python3/Python-$RELEASE
-cd ~
+cd /usr/bin
+sudo rm python
+sudo ln -s /usr/local/bin/python3.9 python
+python --version
