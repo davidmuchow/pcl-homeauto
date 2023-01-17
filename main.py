87c60c1ce69186505eea7745115885e7eba2f0a6
David Muchow <93739747+davidmuchow@users.noreply.github.com>
2023-01-16 22:46:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -65,30 +65,37 @@ MPU_Init()
 
 print (" Reading Data of Gyroscope and Accelerometer")
 
+activated = False
+
 while True:
 	
 	#Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
+   acc_x = read_raw_data(ACCEL_XOUT_H)
+   acc_y = read_raw_data(ACCEL_YOUT_H)
+   acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+   #Read Gyroscope raw value
+   gyro_x = read_raw_data(GYRO_XOUT_H)
+   gyro_y = read_raw_data(GYRO_YOUT_H)
+   gyro_z = read_raw_data(GYRO_ZOUT_H)
 	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
+   #Full scale range +/- 250 degree/C as per sensitivity scale factor
+   Ax = acc_x/16384.0
+   Ay = acc_y/16384.0
+   Az = acc_z/16384.0
+
+   Gx = gyro_x/131.0
+   Gy = gyro_y/131.0
+   Gz = gyro_z/131.0
+
+   print(int(time.time() - start_time))
+   print("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)
+
+   if abs(Az) - .4 > 0 and not activated:
+      activated = True
+      print("machine activate")
    
-	print(int(time.time() - start_time))
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-	sleep(10)
+   sleep(10)
 
 def startup():
    print("booting up...")
