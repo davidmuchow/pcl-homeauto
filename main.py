43fdadc9ae2a6ad14039103249fd8c8ccf1f762e
David Muchow <93739747+davidmuchow@users.noreply.github.com>
2023-01-20 22:30:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def animate(i, xs, ys):
    # Format plot
    plt.xticks(rotation=45, ha='right')
    plt.subplots_adjust(bottom=0.30)
-   plt.title('TMP102 Temperature over Time')
-   plt.ylabel('Temperature (deg C)')
+   plt.title('Z Gyro')
+   plt.ylabel('Acceleration in G')
 
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
 plt.show()
