9fcc8b85d3ebe8b5caa913829fdf382f9233a9ac
David Muchow <93739747+davidmuchow@users.noreply.github.com>
2023-01-30 22:19:29 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ pip3 install gpiozero
 pip3 install datetime
 pip3 install smbus2
 pip install git+https://github.com/DrGFreeman/IFTTT-Webhook.git
+pip install rpio
+pip install pigpio
+pip install RPi.GPIO
 sudo apt install python3-matplotlib python3-tk
 sudo pip3 install matplotlib pytk
