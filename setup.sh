bd90c3ac1cea7c55742d535ac10dc9bb680edd8e
David Muchow <93739747+davidmuchow@users.noreply.github.com>
2023-02-06 21:47:34 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ pip install git+https://github.com/DrGFreeman/IFTTT-Webhook.git
 pip install rpio
 pip install pigpio
 pip install RPi.GPIO
+pip install openpyxl
 sudo apt install python3-matplotlib python3-tk
 sudo pip3 install matplotlib pytk
